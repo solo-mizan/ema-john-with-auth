2541883caf82405a477c0be39335b58a4eab2792
solo-mizan <mizanbd.eee@gmail.com>
2022-04-14 23:58:41 +0600

    !user state added for RequireAuth

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
 
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
+
     const [user] = useAuthState(auth);
-    return (
-        <div>
-            
-        </div>
-    );
+    const location = useLocation();
+
+    if (!user) {
+        return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>
+    }
+
+    return children;
 };
 
 export default RequireAuth;
